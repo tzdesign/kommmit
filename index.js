bebb71a3055ac8cbe85b1a23316206784b057022
Tobias Zimmermann <info@tzdesign.de>
2020-04-22 18:27:33 +0200

    rename

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ const inquirer = require("inquirer");
 
 const { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY } = process.env;
 
-if (
-  AWS_ACCESS_KEY_ID === undefined ||
-  AWS_SECRET_ACCESS_KEY === undefined
-) {
+if (AWS_ACCESS_KEY_ID === undefined || AWS_SECRET_ACCESS_KEY === undefined) {
   console.log(
     `please define the envoirment variables ${`AWS_ACCESS_KEY_ID`.red} and ${
       `AWS_SECRET_ACCESS_KEY`.red
